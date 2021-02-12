3be54aa2a076ad7752ca2d613a540978166eb9b7
Ageev Dmitry <51228454+AgeevDmitryMinsk@users.noreply.github.com>
2021-02-12 19:15:51 +0300

    написать код к проектную работу-5 перед отправкой на первое ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,96 +1,138 @@
-console.log('проверим в консоли подключился ли JS!!!'); //проверим в консоли подключился ли JS!!!
-// проектная работа -4, определяем через let переменные с которыми будем работать (спринт 4 - let - когда переменную нужно перезаписать)
-// Если переменная не перезаписывает своё значение, она объявлена через const - checklist-требование 5ой проектной работы
-// const - когда переменную объявляешь и ее не нужно перезаписывать. Исправил let на const
-const popup = document.querySelector('.popup'); // выбираем в проекте класс "Попап-окна"
-const editButton = document.querySelector('.profile__edit-button'); // выбираем в проекте класс кнопки "редактировать"
-const closeButton = document.querySelector('.popup__button-close');// выбираем в проекте класс кнопки "закрыть popup"
-const saveButton = document.querySelector('.popup__button-save'); // выбираем в проекте класс кнопки "сохранить popup"
-const profileTitle = document.querySelector('.profile__title'); // выбираем в проекте класс заголовка секции "Профиль"
-const profileSubtitle = document.querySelector('.profile__subtitle'); // выбираем в проекте класс подзаголовка секции "Профиль"
-const popupName = document.querySelector('.popup__input_name'); //выбираем в проекте класс первого поля ввода "Имя" в popup
-const popupOccupation = document.querySelector('.popup__input_occupation'); //выбираем в проекте класс первого поля ввода "О себе" в popup
-const formElement = document.querySelector('.form'); //выбираем в проекте класс "form"
-//проектная работа -5, добавим template в html:
-const templateElement = document.querySelector('.card'); // выбираем в проекте класс шаблона секции карточек-изображений;*
-const cardsContainer = document.querySelector('.elements'); // выбираем класс списка изображений в шаблоне;*
+const templateElement = document.querySelector('.card'); // выбираем в проекте класс шаблона секции изображений;
+const editButton = document.querySelector('.profile__edit-button'); // выбираем в проекте класс кнопки "Войти";
+const closeButton = document.querySelector('.popup__button-close'); // выбираем в проекте класс кнопки "Закрыть";
+const popup = document.querySelector('.popup'); // выбираем в проекте класс "Попап-окна";
+const form = document.querySelector('.popup__form'); // выбираем в проекте класс формы в "Попап-окне";
+const inputName = form.querySelector('.popup__input_data_name'); // выбираем в проекте класс первого поля ввода формы в "Попап-окне";
+const inputAbout = form.querySelector('.popup__input_data_about-yourself'); // выбираем в проекте класс второго поля ввода формы в "Попап-окне";
+const profileTitle = document.querySelector('.profile__title'); // выбираем в проекте класс заголовка секции "Профиль";
+const profileSubtitle = document.querySelector('.profile__subtitle'); // выбираем в проекте класс подзаголовка секции "Профиль";
+const cardsContainer = document.querySelector('.elements'); // выбираем класс списка изображений в шаблоне;
+const addButton = document.querySelector('.profile__add-button'); // выбираем в проекте класс кнопки "Добавить";
+const addElementPopup = document.querySelector('.popup_content_add-element'); // выбираем в проекте модификатор "Попап-окна";
+const closeButtonAddElementPopup = document.querySelector('.popup__button-close_place_add-element'); // выбираем в проекте модификатор кнопки "Закрыть";
+const inputTitle = document.querySelector('.popup__input_data_title'); // выбираем в проекте класс поля ввода "Название" формы в попап-окне "Добавить элемент";
+const inputLink = document.querySelector('.popup__input_data_link'); // выбираем в проекте класс второго поля ввода "Ссылка" формы в попап-окне "Добавить элемент";
+const newElementForm = document.querySelector('.popup__form_place_add-element'); // выбираем в проекте модификатор формы в "Попап-окне";
+const imagePopup = document.querySelector('.popup_place_image-popup'); // выбираем в проекте класс "Попап-окна";
+const photoImagePopup = document.querySelector('.popup__image'); // выбираем в проекте класс изображения "Попап-окна";
+const captionImagePopup = document.querySelector('.popup__caption'); // выбираем в проекте класс подписи к изображению "Попап-окна";
+const closeButtonImagePopup = document.querySelector('.popup__button-close_place_image-popup'); // выбираем в проекте класс кнопки "Закрыть";
+const overlay = document.querySelector('.project-area'); // выбираем в проекте класс тега <body>;
+const addPopupForm = document.querySelector('.popup__form_place_add-element'); // выбираем в проекте класс формы в "Попап-окне" добавления нового элемента;
 
 
-editButton.addEventListener('click', () => {// при нажатии/кликании мышкой на openButton произойдет:
-console.log('нажал кнопку editButton')
-popup.classList.add('popup_opened'); // к классу popup добавился класс popup_opened, прописанный в CSS, всплывет popup! - магия!!!)
-})
+const openImagePopup = (evt) => { // объявляем стрелоччную функцию, реализующую открытие попап-окна для просмотра выбранного изображения XXL;
+  const captionValue = evt.target.closest('.element'); // присваиваем переменной класс карточки, по изображению которой кликнул пользователь;
+  openPopup(imagePopup); // добавляем классу "Попап-окна" модификатор, реализующий видимость блока;
+  photoImagePopup.src = evt.target.src; // присваиваем атрибуту изображения попап-окна значение атрибута выбранного изображения карточки;
+  captionImagePopup.textContent = captionValue.textContent; // присваиваем текстовому содержимому подписи к изображению попап-окна текстовое содержимое заголовка выбранной карточки;
+} // ОБРАТИЛ ВНИМАНИЕ, ЧТО ЕСЛИ СТРЕЛОЧНУЮ ФУНКИЮ openImagePopup, removeCard ИЛИ changeLikeButtonColor РАЗМЕСТИТЬ В КОНЦЕ ФАЙЛА, ТО САЙТ НЕ ПОДНИМАЕТСЯ)))
+  // НО ЕСЛИ ИЗ СТРЕЛОЧНОЙ ФУНКЦИИ (=>) СДЕЛАТЬ ПРОСТУЮ, ОБЪЯВЛЕННУЮ ЧЕРЕЗ function openImagePopup() ТО ЕЕ МОЖНО РАЗМЕЩАТЬ ХОТЬ В КОНЦЕ ФАЙЛА, ОНА ПОДНИМЕТСЯ И САЙТ ЗАРАБОТАЕТ!)
 
-closeButton.addEventListener('click', () => { // при нажатии/кликании мышкой на closeButton произойдет:
-    console.log('нажал кнопку closeButton') // в консоль выводтся сообщение "clicked openButton"
-    popup.classList.remove('popup_opened'); // из класса popup убрал класс popup_opened, прописанный в CSS, 
-                                            // popup схлопывается - магия!!!)    
-})
+const removeCard = (evt) => { // объявляем функцию, реализующую удаление карточки из списка;
+  const targetElement = evt.target.closest('.element'); // присваиваем переменной класс карточки, по кнопке удаления которой кликнул пользователь;
+  const imageCard = targetElement.querySelector('.element__image'); // присваиваем переменной класс узла "Изображение карточки";
+  const removeButton = targetElement.querySelector('.element__remove-button'); // присваиваем переменной класс узла "Кнопка удаления карточки";
+  const likeButton = targetElement.querySelector('.element__button-like'); // присваиваем переменной класс узла "Кнопка Лайк";
+  imageCard.removeEventListener('click', openImagePopup); // снимаем "слушатель", вызывающий функцию "openImagePopup" при нажатии на изображение карточки;
+  removeButton.removeEventListener('click', removeCard); // снимаем "слушатель", вызывающий функцию "removeCard" при нажатии на кнопку "Удалить карточку";
+  likeButton.removeEventListener('click', changeLikeButtonColor); // снимаем "слушатель", вызывающий функцию "changeLikeButtonColor" при нажатии на кнопку "Лайк";
+  targetElement.remove(); // удаляем карточку, по кнопке которой кликнул пользователь;
+}
 
-/*из Вебинара 12/20 Георгия Девяткина "Работа с DOM":*/
-popup.addEventListener('click', (event) => {//при нажатии/кликании мышкой на popup-overlay произойдет:
-    console.log(event.target) // в консоль выводится сообщение о текущем событии (в консоль => кликнутый элемент html)
-    if  (event.target === event.currentTarget) { //тоже самое, что event.target === popup
-        popup.classList.remove('popup_opened');} // из класса popup убрал класс popup_opened, прописанный в CSS
-})
-
-saveButton.addEventListener('click', (event) => {
-    console.log('нажал кнопку сохранить');
-    if  (event.target === event.currentTarget) { //тоже самое, что event.target === saveButton
-        profileTitle.textContent = popupName.value; // перенесем-сохраним набранное в теге input popupName в класс profileTitle в html
-        profileSubtitle.textContent = popupOccupation.value; //перенесем-сохраним набранное в теге input popupOcup в класс profileSubtitle в html
-        popup.classList.remove('popup_opened');} // из класса popup убрал класс popup_opened, прописанный в CSS        
-})
-
-function createCard(item) { // объявляем функцию, формирующую из шаблона в проекте стандарную карточку с изображением, подписью и кнопками;
-    const card = templateElement.content.cloneNode(true); // клонируем содержимое шаблона в проекте;
-    const headerCard = card.querySelector('.element__title'); // присваиваем переменной класс узла "Заголовок карточки";
-    const imageCard = card.querySelector('.element__image'); // присваиваем переменной класс узла "Изображение карточки";
-    const removeButton = card.querySelector('.element__remove-button'); // присваиваем переменной класс узла "Кнопка удаления карточки";
-    const likeButton = card.querySelector('.element__button-like'); // присваиваем переменной класс узла "Кнопка Лайк";
-    likeButton.addEventListener('click', changeLikeButtonColor); // подключаем "слушатель", вызывающий функцию "changeLikeButtonColor" при нажатии на кнопку "Лайк";
-    //removeButton.addEventListener('click', removeCard); // подключаем "слушатель", вызывающий функцию "removeCard" при нажатии на кнопку "Удалить карточку";
-    headerCard.textContent = item.name; // присваиваем текстовому содержимому узла "Заголовок карточки" значение ключа "Имя" из заданного массива;
-    imageCard.src = item.link; // присваиваем атрибуту узла "Изображение карточки" значение ключа "Ссылка" из заданного массива;
-    imageCard.alt = item.name; // присваиваем атрибуту узла "Изображение карточки" значение ключа "Имя" из заданного массива;
-    //imageCard.addEventListener('click', openImagePopup); // подключаем "слушатель", вызывающий функцию "openImagePopup" при нажатии на изображение карточки;
-    return card; // возвращаем в функцию значение сформированной карточки;
-  }
-  
-  function createListCard() { // объявляем функцию, формирующую из заданного массива список карточек с изображением, подписью и кнопками;
-    const listCards = initialCards.map(createCard); // присваиваем переменной массив, сформированный из исходного массива и преобразованный с помощью функции "createCard";
-    cardsContainer.append(...listCards); // вставляем в проект полученный массив и раскладываем его на аргументы;
-  }
-  
-  createListCard(); // вызываем функцию для формирования карточек при загрузке страницы;
-
-  function changeLikeButtonColor(evt) { // объявляем функцию, реализующую нажатие кнопки "Лайк" выбранного изображения;
+const  changeLikeButtonColor = (evt) => { // объявляем функцию, реализующую нажатие кнопки "Лайк" выбранного изображения;
   evt.target.classList.toggle('element__button-like_active'); // добавляем классу кнопки "Лайк" модификатор, реализующий изменение цвета кнопки по клику пользователя;
 }
-  
+
+const createCard = (item) => { // объявляем, используя стролочную функцию, формирующую из шаблона template в проекте стандарную карточку с изображением, подписью и кнопками;
+  const card = templateElement.content.cloneNode(true); // клонируем содержимое шаблона в проекте;
+  const headerCard = card.querySelector('.element__title'); // присваиваем переменной класс узла "Заголовок карточки";
+  const imageCard = card.querySelector('.element__image'); // присваиваем переменной класс узла "Изображение карточки";
+  const removeButton = card.querySelector('.element__remove-button'); // присваиваем переменной класс узла "Кнопка удаления карточки";
+  const likeButton = card.querySelector('.element__button-like'); // присваиваем переменной класс узла "Кнопка Лайк";
+  likeButton.addEventListener('click', changeLikeButtonColor); // подключаем "слушатель", вызывающий функцию "changeLikeButtonColor" при нажатии на кнопку "Лайк";
+  removeButton.addEventListener('click', removeCard); // подключаем "слушатель", вызывающий функцию "removeCard" при нажатии на кнопку "Удалить карточку";
+  headerCard.textContent = item.name; // присваиваем текстовому содержимому узла "Заголовок карточки" значение ключа "Имя" из заданного массива;
+  imageCard.src = item.link; // присваиваем атрибуту узла "Изображение карточки" значение ключа "Ссылка" из заданного массива;
+  imageCard.alt = item.name; // присваиваем атрибуту узла "Изображение карточки" значение ключа "Имя" из заданного массива;
+  imageCard.addEventListener('click', openImagePopup); // подключаем "слушатель", вызывающий функцию "openImagePopup" при нажатии на изображение карточки;
+  return card; // возвращаем в функцию значение сформированной карточки;
+}
+
+const composeListCard = () => { // объявляем, используя стролочную функцию, формирующую из заданного массива список карточек с изображением, подписью и кнопками;
+  const listCards = initialCards.map(createCard); // присваиваем переменной массив, сформированный из исходного массива и преобразованный с помощью функции "createCard";
+  cardsContainer.append(...listCards); // вставляем в проект полученный массив и раскладываем его на аргументы;
+}
+
+composeListCard(); // вызываем функцию для формирования карточек при загрузке страницы;
+
+
+const openEditPopup = () => { // объявляем функцию, реализующую открытие "Попап-окна";
+  openPopup(popup); // добавляем классу "Попап-окна" модификатор, реализующий видимость блока;
+  inputName.value = profileTitle.textContent; // присваиваем полю ввода "Имя" формы "Попап-окна" текстовое содержимое заголовка секции "Профиль";
+  inputAbout.value = profileSubtitle.textContent; // присваиваем полю ввода "О себе" формы "Попап-окна" текстовое содержимое подзаголовка секции "Профиль";
+}
+
+editButton.addEventListener('click', openEditPopup); // подключаем "слушатель", вызывающий функцию openClosePopup при нажатии на кнопку "Войти";
+
+const openPopup = (item) => { // объявляем функцию с аргументом, реализующую открытие любого "Попап-окна";
+  item.classList.add('popup_active'); // добавляем классу "Попап-окна" модификатор, реализующий видимость блока; popup_active
+  overlay.addEventListener('keydown', handleKeydown); // подключаем "слушатель", вызывающий функцию handleKeydown при нажатии на клавишу;
+  item.addEventListener('click', closePopupByOverlayClick); // подключаем слушатель, реализующий закрытие попап-окон при клике по оверлею;
+}
+const handleKeydown = (evt) =>{ // объявляем функцию, реализующую закрытие любого "Попап-окна" при нажатии клавиши "Esc";
+  const activePopup = document.querySelector('.popup_active'); // находим в проекте класс активного попап-окна;
+  if (evt.key === 'Escape') { // если нажата клавиша Esc -
+    closePopup(activePopup); // вызываем функцию closePopup;
+  }
+}
+
+const closePopupByOverlayClick = (evt) => { //объявляем, используя стролочную функцию, закрывающую popup при нажатии по overlay;
+  const activePopup = document.querySelector('.popup_active'); // находим в проекте класс активного попап-окна;
+  if (evt.target.classList.contains('popup__container') || // если класс элемента, по которому кликнули, "popup__container"
+      evt.target.classList.contains('popup')) { // или "popup" -
+    closePopup(activePopup); // вызывает функцию closePopup;
+  }
+}
+
+const closePopup = (item) => { // объявляем стрелочную функцию с аргументами, реализующую закрытие любого "Попап-окна";
+  item.classList.remove('popup_active'); // удаляем у класса "Попап-окна" модификатор, реализующий видимость блока; popup_active
+  overlay.removeEventListener('keydown', handleKeydown); // удаляем "слушатель", вызывающий функцию handleKeydown при нажатии на клавишу;
+  item.removeEventListener('click', closePopupByOverlayClick); // удаляем "слушатель", вызывающий функцию closePopupByOverlayClick при клике по оверлею;
+}
+
+
+
+const openAddElementPopup = () => { // объявляем функцию, реализующую открытие "Попап-окна МЕСТО";
+  openPopup(addElementPopup); // добавляем классу "Попап-окна" модификатор, реализующий видимость блока;
+  addPopupForm.reset(); // удаляем данные  формы при открытии попапа;
+}
+
+
+const handleFormSubmit = (evt) => { // объявляем функцию, реализующую сохранение значений полей ввода данных и отправку формы;
+  evt.preventDefault(); // отменяем стандартную отправку формы;
+  profileTitle.textContent = inputName.value; // заменяем текстовое содержимое заголовка секции "Профиль" значением поля ввода "Имя" формы "Попап-окна";
+  profileSubtitle.textContent = inputAbout.value; // заменяем текстовое содержимое подзаголовка секции "Профиль" значением поля ввода "О себе" формы "Попап-окна";
+  closePopup(popup); // реализуем автоматическое закрытие "Попап-окна";
+}
+
+const handleAddElementFormSubmit = (evt) => { // объявляем функцию, реализующую сохранение значений полей ввода данных и создание новой карточки;
+  evt.preventDefault(); // отменяем стандартную отправку формы;
+  const NewCardTitle = inputTitle.value; // присваиваем переменной значение, введенноё пользователем в поле "Название";
+  const NewCardLink = inputLink.value; // присваиваем переменной значение, введенноё пользователем в поле "Ссылка";
+  const NewElement = createCard({ name: NewCardTitle, link: NewCardLink }); // присваиваем переменной значение новой сформированной карточки;
+  cardsContainer.prepend(NewElement); // вставляем новую карточку в начало списка;
+  closePopup(addElementPopup); // закрываем попап-окно;
+}
 
 
 
 
+addButton.addEventListener('click', openAddElementPopup); // подключаем "слушатель", вызывающий функцию "openAddElementPopup" при нажатии на кнопку "Добавить элемент";
+closeButtonAddElementPopup.addEventListener('click', () => closePopup(addElementPopup)); // подключаем "слушатель", вызывающий функцию "closePopup" при нажатии на кнопку "Закрыть";
+newElementForm.addEventListener('submit', handleAddElementFormSubmit); // подключаем "слушатель", вызывающий функцию handleAddElementFormSubmit при нажатии на кнопку "Создать";
+closeButtonImagePopup.addEventListener('click', () => closePopup(imagePopup)); // подключаем "слушатель", вызывающий функцию "closePopup" при нажатии на кнопку "Закрыть";
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*Функция-обработчик срабатывает в момент отправки формы, когда все обязательные поля заполнены. К сожалению,
- при успешной отправке формы и отсутствующем атрибуте action страница перезагружается — это называется 
- стандартным событием. Чтобы такого поведения не происходило — передайте в функцию-обработчик параметр evt.
- В самом начале тела функции вызовите метод evtent.preventDefault() — это отменит стандартное событие.
- Подробнее о стандартных событиях и об их отмене расскажем в следующих спринтах, а сейчас — переходите к заданиям: */
-formElement.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log('Форма отправлена');
-}); 
+closeButton.addEventListener('click', () => closePopup(popup)); // подключаем "слушатель", вызывающий функцию "closePopup" при нажатии на кнопку "Закрыть";
+form.addEventListener('submit', handleFormSubmit); // подключаем "слушатель", вызывающий функцию handleFormSubmit при нажатии на кнопку "Сохранить";
--- scripts/initial-cards.js
+++ scripts/initial-cards.js
@@ -1,29 +1,26 @@
 const initialCards = [ // присваиваем переменной массив, содержащий объекты с данными, необходимыми для формирования карточек с изображением;
-    {
-      name: 'Сфинкс, Гиза, Египет',
-      link: 'https://images.unsplash.com/photo-1486150702851-498e985c2efc?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80'
-      //link: './images/Egypt-2.jfif' - было в проектной работе-4, но точную фотку на unsplash.com не нашел 
-    },
-    {
-      name: 'Париж, Франция',
-      link: 'https://images.unsplash.com/photo-1611223155743-b06c14e05c2c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-      //link: './images/Paris.jfif' - было в проектной работе-4, но точную фотку на unsplash.com не нашел 
-    },
-    {
-      name: 'Франкфурт, Германия',
-      link: 'https://images.unsplash.com/photo-1525367063161-61df4bf6100a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-      // ./images/Frankfurt.jfif - было в проектной работе-4, но точную фотку на unsplash.com не нашел 
-    },
-    {
-      name: 'Нью-Йорк, США',
-      link: 'https://images.unsplash.com/photo-1558898478-433cceebd374?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-    },
-    {
-      name: 'Москва, Россия',
-      link: 'https://gdb.rferl.org/5B283B8C-2A55-4BE4-A41F-93547DB8D07A_cx0_cy4_cw0_w1023_r1_s.jpg'
-    },
-    {
-      name: 'Минск, Беларусь',
-      link: 'https://static.dw.com/image/54510761_303.jpg'
-    }
-  ];
+  {
+    name: 'Египет',
+    link: 'https://images.unsplash.com/photo-1602791191774-d34f0367f1e9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80'
+  },
+  {
+    name: 'Франция',
+    link: 'https://images.unsplash.com/photo-1601988628551-6fd46e168930?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80'
+  },
+  {
+    name: 'Германия',
+    link: 'https://images.unsplash.com/photo-1596996957514-cbde0edba75c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80'
+  },
+  {
+    name: 'США',
+    link: 'https://images.unsplash.com/photo-1520885161078-041b9f6ad3ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80'
+  },
+  {
+    name: 'Беларусь',
+    link: 'https://images.unsplash.com/photo-1601921255435-8061b34d7229?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1348&q=80'
+  },
+  {
+    name: 'Россия',
+    link: 'https://topdialog.ru/wp-content/uploads/2021/01/is_i63f1369.jpg'
+  }
+];
